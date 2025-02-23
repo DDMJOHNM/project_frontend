a7458d6b3064c2e2a43005d995bcbca57bffc7bb
John Mason <jmason176@gmail.com>
2025-02-23 15:21:41 +1300

    Initial Commit

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import Header from "./components/header/header"
+import './globals.css'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+         <Header />
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
---
+++ app/teams/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Teams() {
+    return (
+      <div className="container mx-auto py-24">
+        <div className="min-h-7 min-w-1 rounded overflow-hidden shadow-lg"> 
+        <h2 className="text-center" >Teams</h2>
+       </div>
+      </div>
+    );
+  }
+  
