6ee635a8a0f63087a3816312fd53042e82af1b48
John Mason <johnmason@Johns-iMac.local>
2025-09-23 21:14:30 +1200

    Migrate from GitLab to GitHub with AWS S3 deployment workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  // Images must be handled differently for static exports
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
